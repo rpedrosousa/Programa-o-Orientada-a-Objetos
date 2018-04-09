aa69d1435f6bd4c0e69331be37d1c1847559a5e5
Pedro <pedrosousa.pass@gmail.com>
2018-04-09 14:32:19 +0100

    ficha 4 feita
    ficha 4 feita

--- ficha 4/ficha 4 ex 7.cpp
+++ ficha 4/ficha 4 ex 7.cpp
@@ -5,13 +5,24 @@
 #include <string>
 using namespace std;
 
-int num1,num2m,i;
+int num1,num2,i,x;
 
 main (){
-	cout<< "Introduza um numero: ";
+	cout<< "Introduza o numero: ";
 	cin >> num1;
 	cout<< "Introduza um outro numero: ";
 	cin >> num2;
+	
+	for(x=0;num2>num1;x++){
+		cout<<"Numero Invalido"<<endl;
+		cout<< "Introduza o numero: ";
+		cin >> num1;
+		cout<< "Introduza um outro numero: ";
+		cin >> num2;
+ 	}
+	for(i=num2+1;i<num1;i++){
+		cout<<i<<endl;
+	}
 	
 	
 }
